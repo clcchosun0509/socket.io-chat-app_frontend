44278e025fddeed1f29fde9b638ba921483f2809
박건우 <clcchosun0509@naver.com>
2022-11-30 02:03:37 +0900

    feat: finish profile basic ui

---
+++ src/components/ui/ListItem.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  title: string;
+  content: string;
+}
+const ListItem = ({ title, content }: Props) => {
+  return (
+    <div className="flex flex-col p-3 py-4">
+      <p className="pb-1 font-notoSans font-light">{title}</p>
+      <p className="font-notoSans font-thin text-sm text-gray-500">{content}</p>
+    </div>
+  );
+};
+
+export default ListItem;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,23 +1,28 @@
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, useNavigate } from "react-router-dom";
 import Background from "../components/UI/Background";
 import Card from "../components/UI/Card";
 import BottomNavBar from "../components/BottomNavBar";
 import { User } from "../service/type";
 import TopNavBar from "../components/TopNavBar";
+import ListItem from "../components/UI/ListItem";
 
 type ContextProps = {
   user: User;
 };
 const Profile = () => {
+  const navigate = useNavigate();
   const { user } = useOutletContext<ContextProps>();
-  console.log("Profile user", user);
 
   return (
     <Background>
       <Card className="flex flex-col justify-between divide-y-2">
         <TopNavBar text="프로필" />
-        <div className="h-full">Profile</div>
-        <BottomNavBar active="profile" />
+        <div className="h-full">
+          <ListItem title="닉네임" content={user.username} />
+          <ListItem title="이메일" content={user.email} />
+          <ListItem title="아이디" content={user.id} />
+        </div>
+        <BottomNavBar active="profile" onClickChat={() => navigate("/chatroom")}/>
       </Card>
     </Background>
   );
